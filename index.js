ac2ccdbbeed57646f249f353e9676d2f3c32e3d3
Mateus <mateusoliveira469@gmail.com>
2022-04-28 12:02:41 -0400

    db-connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { MongoCLient } from 'mongodb';
 
 const app = express ();
 app.use(cors());
@@ -8,7 +9,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const mongoCLient = new MongoCLient('mongodb://localhost:27017');
+let db = null;
+
+const promise = mongoCLient.connect();
+promise.then(res  => {
+    db = mongoCLient.db('uol_chat');
+    console.log("Data base is connected!");
+});
+
+db.collection('users').insertOne({
+    name: 'testName',
+    lestStatus: 55555
+});
+
+
+
 
 app.listen(5000, () => {
     console.log('API running!');
-})
+});
