a47eb7f02cf6fffe93064c415c584b72d2c21ba8
Mateus <mateusoliveira469@gmail.com>
2022-05-02 14:04:21 -0400

    post-&&-get-messages

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const participantSchema = joi.object({
+
     name: joi.string().required()
+
 });
 const messageSchema = joi.object({
+
     to: joi.string().required(),
     text: joi.string().required(),
     type: joi.string().valid('message', 'private_message')
+
 });
+
+function filterUsersMessages (message, participant) {
+
+    if (message.to === 'Todos' || message.from === participant || message.to === participant) {
+
+        return message;
+
+    }
+
+}
 
 app.post('/participants', async (req, res) => {
 
@@ -105,23 +119,80 @@ app.post('/messages', async (req, res) => {
     const from = req.headers;
 
     if (isValidMessage.error) {
+
         return res.sendStatus(422);
+
     }
 
     try {
 
         const mongoClient = new MongoClient(process.env.MONGO_URI);
         await mongoClient.connect();
+        const participantsCollection = mongoClient.db('bate-papo-uol-chat').collection('participants');
+        const messagesCollection = mongoClient.db('bate-papo-uol-chat').collection('messages');
+
+        const isValidParticipant = await participantsCollection.findOne({ name: from });
+
+        if (!isValidParticipant) {
+
+            return res.sendStatus(422);
+
+        }
+
+        await messagesCollection.insertOne({
+
+            ...message,
+            from,
+            time: dayjs().format('HH:mm:ss')
+
+        });
+
+        await mongoClient.close();
+        res.sendStatus(201);
 
     } catch (e) {
 
-        res.sendStatus(500, e);
+        res.sendStatus(500);
         
     }
 
 });
 
 app.get('/messages', async (req, res) => {
+
+    const messagesCap = (req.query.limit);
+    const participant = req.headers.user;
+
+    try {
+
+        const mongoClient = new MongoClient(process.env.MONGO_URI);
+        await mongoClient.connect();
+        
+        const messagesCollection = mongoClient.db('bate-papo-uol-chat').collection('messages');
+        const messages = await messagesCollection.find({}).toArray(); 
+
+        const usersMessages = messages.filter((message) => {
+
+            filterUsersMessages(message, participant);
+
+        });
+
+        await mongoClient.close();
+
+        if (messagesCap !== null) {
+            
+            return res.send(usersMessages).slice(-messagesCap);
+
+        }
+
+        res.send(usersMessages);
+    
+    } catch (e) {
+
+        console.log(e);
+        res.sendStatus(500);
+
+    }
 
 });
 
